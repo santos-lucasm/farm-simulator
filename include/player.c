e2bbeda3f4dfa92eebdeb82926116238489f7a52
santos-lucasm <santos.lucasmmatheus@gmail.com>
2022-07-18 00:56:35 -0300

    Add player arrow controls

--- include/player.c
+++ include/player.c
@@ -7,14 +7,21 @@ player_t* create_player()
    p->pos = (Vector2) {0.0f, 0.0f};
    p->width = 20.f;
    p->height = 30.f;
-   p->speed = 10.0f;
+   p->speed = 50.0f;
    p->color = RED;
    return p;
 }
 
 void update_player(player_t * self)
 {
-  self->pos.x = self->pos.x + self->speed  * GetFrameTime();
+   if(IsKeyDown(KEY_RIGHT))
+      self->pos.x += self->speed * GetFrameTime();
+   if(IsKeyDown(KEY_LEFT))
+      self->pos.x -= self->speed * GetFrameTime();
+   if(IsKeyDown(KEY_UP))
+      self->pos.y -= self->speed * GetFrameTime();
+   if(IsKeyDown(KEY_DOWN))
+      self->pos.y += self->speed * GetFrameTime();
 }
 
 void draw_player(player_t * self) 
