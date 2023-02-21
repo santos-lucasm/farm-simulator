2f8fc469647b6b2afb3df7dc5906f2e4e508387e
santos-lucasm <santos.lucasmmatheus@gmail.com>
2023-02-20 21:41:43 -0300

    [] Add malloc validation to world camera creation

--- src/camera.c
+++ src/camera.c
@@ -2,9 +2,13 @@
 //------------------------------------------------------------------------------------
 world_camera_t * create_world_camera(const Vector2 targetPos)
 {
-    world_camera_t * wc = malloc(sizeof(world_camera_t));
-    wc->camera = create2D_camera(targetPos);
-    wc->entryZoomIn = true;
+    world_camera_t * wc = (world_camera_t *) malloc( sizeof(world_camera_t) );
+    if(NULL != wc)
+    {
+        wc->camera = create2D_camera(targetPos);
+        wc->entryZoomIn = true;
+    }
+    return wc;
 }
 //------------------------------------------------------------------------------------
 void destroy_world_camera(world_camera_t * self)
@@ -15,7 +19,7 @@ void destroy_world_camera(world_camera_t * self)
 //------------------------------------------------------------------------------------
 Camera2D * create2D_camera(const Vector2 targetPos)
 {
-    Camera2D * c = malloc(sizeof(Camera2D));
+    Camera2D * c = (Camera2D *) malloc( sizeof(Camera2D) );
     if(NULL != c)
     {
         c->target = targetPos;
@@ -31,21 +35,18 @@ void update_camera(world_camera_t * self, const Vector2 targetPos)
     if(NULL != self)
     {
         if(self->entryZoomIn)
+        {
             event_entry_zoomIn(self);
-        
+        }
         self->camera->target = targetPos;
     }
 }
 //------------------------------------------------------------------------------------
 void event_entry_zoomIn(world_camera_t * self)
 {
-    if(3.0f < self->camera->zoom)
+    if(3.0f <= self->camera->zoom)
     {
         self->camera->zoom = 3.0f;
-        self->entryZoomIn = false;
-    }
-    else if(3.0f == self->camera->zoom)
-    {
         self->entryZoomIn = false;
     }
     else
